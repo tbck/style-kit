5a99c78425378df0be6eede1b6507dad1fe27f5b
Tyler Buck <tyler.buck@maine.edu>
2016-05-22 23:04:05 -0400

    Upgrade stylelint and fix linter errors

--- test/lint.js
+++ test/lint.js
@@ -2,11 +2,14 @@ var fs = require('fs');
 var path = require('path');
 var postcss = require('postcss');
 var glob = require('glob');
+var stylelint = require('stylelint');
+var bemLinter = require('postcss-bem-linter');
+var reporter = require('postcss-reporter');
 
 var processors = [
-  require('postcss-bem-linter')({ preset: 'suit' }),
-  require('stylelint'),
-  require('postcss-reporter')
+  bemLinter({ preset: 'suit' }),
+  stylelint(),
+  reporter({ clearMessages: true })
 ];
 
 var files = glob.sync('./lib/**/*.css');
